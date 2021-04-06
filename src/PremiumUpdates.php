839ca0b421ea0cc5ad6b361a773aa9515e38a0b3
Alain Schlesser <alain.schlesser@gmail.com>
2021-04-06 12:15:13 +0100

    Fake admin request

--- src/PremiumUpdates.php
+++ src/PremiumUpdates.php
@@ -4,19 +4,41 @@ namespace Cloudways\PremiumUpdates;
 
 use stdClass;
 use WP_CLI;
+use WP_Session_Tokens;
 
 final class PremiumUpdates {
 
+    /**
+     * Debugging group to use for this package.
+     *
+     * @var string
+     */
     const DEBUG_GROUP = 'premium-updates';
+
+    /**
+     * Array of commands to intercept.
+     *
+     * @var array<array>
+     */
+    const COMMANDS_TO_INTERCEPT = [
+        [ 'plugin', 'list' ],
+        [ 'plugin', 'update' ],
+    ];
 
     /**
      * Register the Premium Updates package with WordPress & WP-CLI.
      */
     public function register()
     {
-        \add_filter( 'pre_set_site_transient_update_plugins', [ $this, 'pre_set_site_transient_update_plugins' ] );
+        if ( $this->is_command_to_intercept() ) {
+            $this->fake_admin_request();
 
-        WP_CLI::debug( 'Cloudways premium updates package registered', self::DEBUG_GROUP);
+            WP_CLI::add_wp_hook( 'pre_set_site_transient_update_plugins', [ $this, 'pre_set_site_transient_update_plugins' ] );
+
+            WP_CLI::debug( 'Cloudways premium updates package registered', self::DEBUG_GROUP);
+        } else {
+            WP_CLI::debug( 'Cloudways premium updates package registration skipped', self::DEBUG_GROUP);
+        }
     }
 
     /**
@@ -65,5 +87,86 @@ final class PremiumUpdates {
         }
 
         return WP_CLI::colorize( implode( '; ', $output ) );
+    }
+
+    /**
+     * Fake the environment of an administration request running on the WP backend.
+     */
+    public function fake_admin_request()
+    {
+        if ( defined( 'WP_ADMIN' ) ) {
+            if ( ! WP_ADMIN ) {
+                WP_CLI::warning( 'Premium updates package could not fake admin request.' );
+            }
+
+            return;
+        }
+
+        WP_CLI::debug( 'Faking an admin request', self::DEBUG_GROUP );
+
+        // Define `WP_ADMIN` as being true. This causes the helper method `is_admin()` to return true as well.
+        define( 'WP_ADMIN', true );
+
+        // Set a fake entry point to ensure wp-includes/vars.php does not throw notices/errors.
+        // This will be reflected in the global `$pagenow` variable being set to 'wp-cli-fake-admin-file.php'.
+        $_SERVER['PHP_SELF'] = '/wp-admin/wp-cli-fake-admin-file.php';
+
+        // Bootstrap the WordPress administration area.
+        WP_CLI::add_wp_hook( 'init', function () {
+            global $wp_db_version, $_wp_submenu_nopriv;
+
+            // Make sure we don't trigger a DB upgrade as that tries to redirect the page.
+            $wp_db_version = (int) get_option( 'db_version' );
+
+            // Ensure WP does not iterate over an undefined variable in user_can_access_admin_page().
+            if ( ! isset( $_wp_submenu_nopriv ) ) {
+                $_wp_submenu_nopriv = [];
+            }
+
+            $this->log_in_as_admin_user();
+
+            require_once ABSPATH . 'wp-admin/admin.php';
+        }, 0 );
+    }
+
+    /**
+     * Check whether the current WP-CLI command is amongst those we want to intercept and adapt.
+     *
+     * As we need to drastically alter the environment under which these commands are run, we want to bail early in case
+     * an unrelated command is run.
+     *
+     * @return bool Whether the current command should be intercepted.
+     */
+    private function is_command_to_intercept()
+    {
+        $command = WP_CLI::get_runner()->arguments;
+
+        if ( in_array( $command, self::COMMANDS_TO_INTERCEPT ) ) {
+            WP_CLI::debug( 'Detected a command to be intercepted: ' . implode( ' ', $command ), self::DEBUG_GROUP );
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Ensure the current request is done under a logged-in administrator account.
+     *
+     * A lot of premium plugins/themes have their custom update routines locked behind an is_admin() call.
+     */
+    private function log_in_as_admin_user()
+    {
+        $admin_user_id = 1;
+
+        // TODO: Add logic to find an administrator user.
+
+        wp_set_current_user( $admin_user_id );
+
+        $expiration = time() + DAY_IN_SECONDS;
+        $manager    = WP_Session_Tokens::get_instance( $admin_user_id );
+        $token      = $manager->create( $expiration );
+
+        $_COOKIE[AUTH_COOKIE]        = wp_generate_auth_cookie( $admin_user_id, $expiration, 'auth', $token );
+        $_COOKIE[SECURE_AUTH_COOKIE] = wp_generate_auth_cookie( $admin_user_id, $expiration, 'secure_auth', $token );
     }
 }
